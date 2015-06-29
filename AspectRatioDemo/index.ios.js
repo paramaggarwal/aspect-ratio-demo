835e9501bd6ab8dc0c06105305f5e33e26a693ef
Param Aggarwal <paramaggarwal@gmail.com>
2015-06-29 08:52:58 +0530

    additions to demo

--- AspectRatioDemo/index.ios.js
+++ AspectRatioDemo/index.ios.js
@@ -47,14 +47,61 @@ var AspectRatioDemo = React.createClass({
           onValueChange={(value) => this.setState({value: value})} />
 
         <View style={{width: viewWidth, backgroundColor: 'white'}}>
-          <Image aspectRatioBox={{
-            width: 612,
-            height: 456,
-          }}
-          source={{
-            uri: "http://edibleapple.com/wp-content/uploads/2009/11/steve-jobs-bill-gates-1991.jpg",
-          }} />
+
+          <Image
+            aspectRatioBox={{
+              width: 612,
+              height: 456,
+            }}
+            source={{
+              uri: "http://edibleapple.com/wp-content/uploads/2009/11/steve-jobs-bill-gates-1991.jpg",
+            }}
+          />
+
+          <View aspectRatioBox={{
+            width: 600,
+            height: 600,
+          }} style={{alignSelf: 'stretch', flex: 1}}>
+            <View style={{flexDirection: 'row', flex: 1}}>
+              <Image
+                aspectRatioBox={{
+                  width: 612,
+                  height: 456,
+                }}
+                style={{margin: 2, flex: 1}}
+                source={{uri: "http://edibleapple.com/wp-content/uploads/2009/11/steve-jobs-bill-gates-1991.jpg"}}
+              />
+              <Image
+                aspectRatioBox={{
+                  width: 612,
+                  height: 456,
+                }}
+                style={{margin: 2, flex: 2}}
+                source={{uri: "http://edibleapple.com/wp-content/uploads/2009/11/steve-jobs-bill-gates-1991.jpg"}}
+              />
+            </View>
+            <View style={{flexDirection: 'row', flex: 1}}>
+              <Image
+                aspectRatioBox={{
+                  width: 612,
+                  height: 456,
+                }}
+                style={{margin: 2, flex: 3}}
+                source={{uri: "http://edibleapple.com/wp-content/uploads/2009/11/steve-jobs-bill-gates-1991.jpg"}}
+              />
+              <Image
+                aspectRatioBox={{
+                  width: 612,
+                  height: 456,
+                }}
+                style={{margin: 2, flex: 2}}
+                source={{uri: "http://edibleapple.com/wp-content/uploads/2009/11/steve-jobs-bill-gates-1991.jpg"}}
+              />
+            </View>
           </View>
+
+
+        </View>
       </ScrollView>
     );
   }
